67705bf9d94245b18ddc88d8780f2bc2f08b0418
epenn <penn@media.mit.edu>
2020-03-26 06:41:05 -0400

    Add a script to handle automated archive commits of data and pages

---
+++ scripts/push-archive.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+repo_path="$1"
+run_dt=`date -u --rfc-3339=seconds`
+
+if [ -z "$repo_path" ]; then
+  echo "usage: run-push-archive.sh REPO_PATH"
+  exit 1;
+fi
+
+cd $repo_path
+
+git add index.html
+git add data-archive/*
+git add page-archive/*
+
+git commit -m "Automated archive update at $run_dt"
+git push origin
+
