4f0b55888a21de6e7df5a36150964f77925a3e25
Nathan Matias <natematias@gmail.com>
2020-04-01 15:24:08 -0400

    updated script to automatically omit embedded fonts

--- scripts/render-report.sh
+++ scripts/render-report.sh
@@ -3,4 +3,5 @@ output_options = list()
 output_options\$includes\$in_header = c('_metatags.html', '_favicon.html')
 rmarkdown::render('templates/covid19-dashboard.Rmd', output_file='../index.html', output_options=output_options)
 EOF
-
+## Remove embedded fonts from the dashboard file
+sed -i 580,610d index.html
